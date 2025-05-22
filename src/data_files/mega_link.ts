79b34847c2eef2bc245ca5a2a8ba6c66f017623b
prem_tiwari <ptiwari2407@gmail.com>
2025-05-22 22:26:34 +0200

    feat(Navbar): Add Profoundness factor under Life Coaching

--- src/data_files/mega_link.ts
+++ src/data_files/mega_link.ts
@@ -42,13 +42,13 @@ export const servicesData = [
   },
 ];
 
-export const successStoriesData = [
+export const profoundnessFactorData = [
   {
     image:
       "https://images.unsplash.com/photo-1544005313-94ddf0286df2?q=80&w=1376&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D8&auto=format&fit=facearea&facepad=2&w=320&h=320&q=80",
-    alt: "Image Description",
+    alt: "Profoundness Factor",
     description:
-      "See how ScrewFast has empowered businesses of all sizes to achieve outstanding results.",
-    learnMoreUrl: "#",
+      "Explore your life's purpose and personal growth journey",
+    learnMoreUrl: "/profoundness/factor",
   },
 ];
