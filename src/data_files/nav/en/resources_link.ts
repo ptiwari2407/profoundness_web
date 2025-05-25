00ca0ac4e3a77a082f21ac97599d25bb2cbb2e88
prem_tiwari <ptiwari2407@gmail.com>
2025-05-25 10:43:16 +0200

    feat(Navbar): Add Resources  under Resources

---
+++ src/data_files/nav/en/resources_link.ts
@@ -0,0 +1,42 @@
+// Define data structures for services and success stories
+export const resourcesData = [
+  {
+    title: "Inner Circle",
+    description:
+      "Dive deep into helpful guides and explanations for all of ScrewFast's features",
+    icon: "guides",
+    url: "#",
+  },
+  {
+    title: "Blog",
+    description:
+      "Supercharge Your Workflow. Seamless integrations with all your favorite tools",
+    icon: "puzzle",
+    url: "#",
+  },
+  {
+    title: "Books",
+    description: "Go beyond tools with ScrewFast's expert services",
+    icon: "rocket",
+    url: "#",
+  },
+  {
+    title: "Recommended Study Lists",
+    description:
+      "Build Smarter, Faster. Experience next-level efficiency with ScrewFast's cutting-edge construction tools",
+    icon: "hammer",
+    url: "#",
+  },
+
+];
+
+export const resourcesFactorData = [
+  {
+    image:
+      "https://images.unsplash.com/photo-1544005313-94ddf0286df2?q=80&w=1376&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D8&auto=format&fit=facearea&facepad=2&w=320&h=320&q=80",
+    alt: "Profoundness Factor",
+    description:
+      "Explore your life's purpose and personal growth journey",
+    learnMoreUrl: "/profoundness/factor",
+  },
+];
