aef7fd0fe4f49cdacd75257a3b5a6f062f1070de
prem_tiwari <ptiwari2407@gmail.com>
2025-05-26 17:14:35 +0200

    SEO: Modified SEO contents

--- src/data_files/constants.ts
+++ src/data_files/constants.ts
@@ -3,12 +3,12 @@ import ogImageSrc from "@images/social.png";
 
 
 export const SITE = {
-  title: "ScrewFast",
+  title: "LifeProfoundness",
   tagline: "Top-quality Hardware Tools",
   description: "ScrewFast offers top-tier hardware tools and expert construction services to meet all your project needs. Start exploring and contact our sales team for superior quality and reliability.",
   description_short: "ScrewFast offers top-tier hardware tools and expert construction services to meet all your project needs.",
-  url: "https://screwfast.uk",
-  author: "Emil Gulamov",
+  url: "https://lifeprofoundness.com",
+  author: "Prem Tiwari",
 };
 
 export const SEO = {
